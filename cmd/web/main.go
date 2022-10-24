feff3e9fc3ddc6fa58776da87930b8aad0f31c84
Руденко Данила Олегович <d.rudenko@inenergy.local>
2022-10-24 17:00:25 +0300

    test

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -12,6 +12,17 @@ func main() {
 	mux.HandleFunc("/snippet", showSnippet)
 	mux.HandleFunc("/snippet/create", createSnippet)
 
+	// Инициализируем FileServer, он будет обрабатывать
+	// HTTP-запросы к статическим файлам из папки "./ui/static".
+	// Обратите внимание, что переданный в функцию http.Dir путь
+	// является относительным корневой папке проекта
+	fileServer := http.FileServer(http.Dir("ui/static"))
+
+	// Используем функцию mux.Handle() для регистрации обработчика для
+	// всех запросов, которые начинаются с "/static/". Мы убираем
+	// префикс "/static" перед тем как запрос достигнет http.FileServer
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
 	//http.HandleFunc("/hello", viewHandler)
 	log.Println("run server")
 	err := http.ListenAndServe("localhost:8080", mux)
